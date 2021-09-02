7ccfc0c0541333fccc643174bf66ae99a659fa70
Strandor <steinar@leikirnir.is>
2021-09-02 13:23:14 +0000

    Update iq.py

--- assignments/basics/iq.py
+++ assignments/basics/iq.py
@@ -2,8 +2,6 @@ mental_age_int = int(input("What is the mental age of the subject: ")) # Do not 
 chronological_age_int = int(input("What is the chronological age of the subject: ")) # Do not change this line
 
 # Fill in the missing code below
-
 iq_score_float = (mental_age_int / chronological_age_int) * 100
-
 
 print("The subject's IQ is", round(iq_score_float)) # Do not change this line
